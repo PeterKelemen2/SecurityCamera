b7fa5cce9dda90ee5b04824d363759a0dd40ad3f
Kelemen Péter <peter.kelemen456@gmail.com>
2024-04-29 11:37:45 +0200

    Added codec and writing to files

--- capture.py
+++ capture.py
@@ -1,5 +1,25 @@
+import datetime
+
 import cv2
 import numpy as np
+
+output_path = "videos"
+# Set the font, scale, color, and thickness
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 0.5
+font_color = (0, 255, 0)  # white color
+line_type = 2
+
+
+def add_text(frame):
+    # Get text size
+    date_time = f"{datetime.datetime.now().strftime("%Y. %m. %d. %H:%M:%S")}"
+    text_size, _ = cv2.getTextSize(date_time, font, font_scale, line_type)
+    text_x = frame.shape[1] - text_size[0] - 10  # 10 pixels from the right edge
+    text_y = frame.shape[0] - 10  # 10 pixels from the bottom
+
+    cv2.putText(frame, date_time, (text_x, text_y), font, font_scale, font_color, line_type)
+    return frame
 
 
 def adjust_frame(frame):
@@ -29,6 +49,18 @@ def set_fixed_white_balance(cap):
 
 def capture_image():
     cap = cv2.VideoCapture(0)
+    w, h, fps = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), 30.0
+    codec = cv2.VideoWriter_fourcc(*'H264')
+
+    print(w, h, fps)
+
+    date = f"{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}"
+
+    global output_path
+    output_path = f"{output_path}/video-{date}.mp4"
+    print(output_path)
+    writer = cv2.VideoWriter(output_path, codec, fps, (w, h))
+    frame_array = []
     if not cap.isOpened():
         print("Unable to open camera")
         return
@@ -65,17 +97,29 @@ def capture_image():
         total_pixels = thresh_diff.size
         percent_change = f"{((significant_changes / total_pixels) * 100):.2f}"
 
-        print(f"{percent_change}")
+        # print(f"{percent_change}")
+
+        if float(percent_change) > 5.0:
+            frame_array.append(frame)
+            cv2.imshow('Frame Difference', frame_diff)
+            cv2.imshow('Thresholded Difference', thresh_diff)
+            if len(frame_array) > 5:
+                for f in frame_array:
+                    f = add_text(f)
+                    writer.write(f)
+                frame_array = []
 
         # Display the difference and the thresholded difference
-        cv2.imshow('Frame Difference', frame_diff)
-        cv2.imshow('Thresholded Difference', thresh_diff)
+        # cv2.imshow('Frame Difference', frame_diff)
+        # cv2.imshow('Thresholded Difference', thresh_diff)
 
         # Update previous frame
         prev_frame = frame.copy()
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            writer.release()
             break
 
     cap.release()
+    # writer.release()
     cv2.destroyAllWindows()
